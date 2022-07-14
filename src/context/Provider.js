fc35b193de7cfef99f5bdcbfa823a986494e4c97
gralmeidan <gralmeidan@gmail.com>
2022-07-14 17:42:22 -0400

    Fixed a bug with the provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -4,16 +4,14 @@ import Context from './Context';
 
 function Provider({ children }) {
   return (
-    <Context.Provider>
+    <Context.Provider value={ {} }>
       {children}
     </Context.Provider>
   );
 }
 
 Provider.propTypes = {
-  children: PropTypes.arrayOf(
-    PropTypes.element.isRequired,
-  ).isRequired,
+  children: PropTypes.element.isRequired,
 };
 
 export default Provider;
