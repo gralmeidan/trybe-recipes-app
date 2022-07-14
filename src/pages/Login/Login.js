8c0e92bf47990c32ae72bffbf4abd204f6c967c6
polidex <lp.igor@gmail.com>
2022-07-14 18:59:15 -0300

    requisitos 2 e 3 prontos

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,7 +3,22 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      login
+      <input
+        data-testid="email-input"
+        type="text"
+        placeholder="E-mail"
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        placeholder="password"
+      />
+      <button
+        data-testid="login-submit-btn"
+        type="submit"
+      >
+        Login
+      </button>
     </div>
   );
 }
