29b011eefd97328d9e7368f174cd7ee76a53515b
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-07-15 10:17:01 -0300

    Arquivo Header.js (Requisito 07) - Concluído

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function Header() {
-  return (
+import profileIcon from '../../images/profileIcon.svg';
+import searchIcon from '../../images/searchIcon.svg';
+
+const Header = ({ title }) => (
+  <header>
     <div>
-      Header
+      <Link to="/profile">
+        <span data-testid="profile-top-btn">
+          <img src={ profileIcon } alt="Profile" />
+        </span>
+      </Link>
     </div>
-  );
-}
+    <div>
+      <h1 data-testid="page-title">
+        { title }
+      </h1>
+    </div>
+    <div>
+      <span data-testid="search-top-btn">
+        <img src={ searchIcon } alt="Search Recipes" />
+      </span>
+    </div>
+  </header>
+);
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+Header.defaultProps = {
+  title: 'Teste',
+};
 
 export default Header;
