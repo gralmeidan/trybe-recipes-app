9e41cea2d1925df5cda401a9a5a3d0fe4e394b7b
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-07-15 10:48:35 -0300

    Adicionado o Header as Páginas/Rotas

--- src/pages/Recipes/Recipes.js
+++ src/pages/Recipes/Recipes.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Header from '../../components';
 
 function Recipes() {
+  // Título apenas para teste
+  const headerTitle = 'Food/Drinks Recipes';
+
   return (
-    <div>
-      Recipes
-    </div>
+    <>
+      <Header title={ headerTitle } />
+      <main id="recipes-page">
+        <h2>Recipes</h2>
+      </main>
+    </>
+
   );
 }
 
