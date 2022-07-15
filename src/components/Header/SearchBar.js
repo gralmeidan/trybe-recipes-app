d1738ff2cfeed25e4c74deadd98c61f959f1c969
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-07-15 13:20:39 -0300

    Requisito 10

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -14,14 +14,58 @@ const SearchBar = () => {
   return (
     <div id="search">
       <form onSubmit={ (e) => handleSubmit(e) }>
-        <input
-          type="text"
-          name="search-input"
-          id="search-input"
-          value={ searchValue }
-          onChange={ ({ target: { value } }) => handleChange(value) }
-          data-testid="search-input"
-        />
+        <div>
+          <input
+            type="text"
+            id="search-input"
+            name="search-input"
+            value={ searchValue }
+            onChange={ ({ target: { value } }) => handleChange(value) }
+            data-testid="search-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="ingredient-search-radio">
+            <input
+              type="radio"
+              id="ingredient-search-radio"
+              name="ingredient-search-radio"
+              data-testid="ingredient-search-radio"
+              value="ingredient"
+            />
+            Ingredient
+          </label>
+          <label htmlFor="name-search-radio">
+            <input
+              type="radio"
+              id="name-search-radio"
+              name="name-search-radio"
+              data-testid="name-search-radio"
+              value="name"
+            />
+            Name
+          </label>
+          <label htmlFor="first-letter-search-radio">
+            <input
+              type="radio"
+              id="first-letter-search-radio"
+              name="first-letter-search-radio"
+              data-testid="first-letter-search-radio"
+              value="first-letter"
+            />
+            First Letter
+          </label>
+        </div>
+        <div>
+          <button
+            type="button"
+            id="exec-search-btn"
+            name="exec-search-btn"
+            data-testid="exec-search-btn"
+          >
+            Search
+          </button>
+        </div>
       </form>
     </div>
   );
