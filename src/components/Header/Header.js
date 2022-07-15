69ee73dd327d18d7ff545fabc01189fc7eba6615
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-07-15 11:09:20 -0300

    Requisito 09

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
 
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 
-const Header = ({ title }) => (
-  <header>
-    <div id="header-profile">
-      <Link to="/profile">
-        <img
-          src={ profileIcon }
-          alt="Profile"
-          data-testid="profile-top-btn"
-        />
-      </Link>
-    </div>
-    <div id="header-title">
-      <h1 data-testid="page-title">
-        { title }
-      </h1>
-    </div>
-    <div id="header-search">
-      <img
-        src={ searchIcon }
-        alt="Search Recipes"
-        data-testid="search-top-btn"
-      />
-    </div>
-  </header>
-);
+const Header = ({ title }) => {
+  const [searchEnable, setSearchEnable] = useState(false);
+
+  const handleSearch = () => {
+    setSearchEnable(!searchEnable);
+  };
+
+  return (
+    <>
+      <header>
+        <div id="header-profile">
+          <Link to="/profile">
+            <img
+              src={ profileIcon }
+              alt="Profile"
+              data-testid="profile-top-btn"
+            />
+          </Link>
+        </div>
+        <div id="header-title">
+          <h1 data-testid="page-title">
+            { title }
+          </h1>
+        </div>
+        <div id="header-search">
+          <button
+            type="button"
+            name="search-top-btn"
+            id="search-top-btn"
+            onClick={ handleSearch }
+            data-testid="search-top-btn"
+          >
+            <img
+              src={ searchIcon }
+              alt="Search Recipes"
+              data-testid="search-top-btn"
+            />
+          </button>
+        </div>
+      </header>
+      { searchEnable && <SearchBar /> }
+    </>
+  );
+};
 
 Header.propTypes = {
   title: PropTypes.string,
