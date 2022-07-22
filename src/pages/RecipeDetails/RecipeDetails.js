97ef67399193f99df99929efdab21837c21d9af3
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-07-22 18:47:58 -0300

    Corrigindo importação Button (RecipeDetails.js)

--- src/pages/RecipeDetails/RecipeDetails.js
+++ src/pages/RecipeDetails/RecipeDetails.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
 
 import PropTypes from 'prop-types';
 import getIngredientAndMeasureList from '../../helpers/getIngredientAndMeasureList';
